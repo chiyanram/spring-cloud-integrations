560a372167683f1b4372d87189e2df609c214693
Ramachandran <ramachandran.murugaian@gmail.com>
2020-04-25 21:56:39 -0600

    removed common

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "Spring Cloud Integrations"
 
-include("spring-cloud-common",
+include(
         "spring-cloud-encrypt-integration",
         "spring-cloud-gateway-service-one",
         "spring-cloud-gateway-service-two",
